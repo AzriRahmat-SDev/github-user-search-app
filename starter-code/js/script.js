40c8206456e9727fb0185e6358322b16b265d4b9
azrirahmat <azrirahmatishardlyworking@gmail.com>
2021-12-29 21:49:20 +0800

    making the thumbnail responsive

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -12,13 +12,68 @@ form.addEventListener('submit', function (e) {
 			console.log(data);
 
 			document.getElementById('result-name').innerHTML = `
-            <p>${data.name}</p>`;
+            <h1>${data.name}</h1>`;
+
+			document.getElementById('result-handle').innerHTML = `
+            <p>@${data.login}</p>`;
+
+			document.getElementById('result-bio').innerHTML = `
+            <p>${data.bio}</p>`;
+			if (data.bio === null) {
+				document.getElementById(
+					'result-bio'
+				).innerHTML = `<p>This profile has no bio  fetch successful</p>`;
+			}
+			//Repo
+			document.getElementById('repo').innerHTML = `<p>${data.public_repos}</p>`;
+
+			//Followers
+			document.getElementById(
+				'followers'
+			).innerHTML = `<p>${data.followers}</p>`;
+
+			//Following
+			document.getElementById(
+				'following'
+			).innerHTML = `<p>${data.following}</p>`;
+
+			//Location
+			document.getElementById('location').innerHTML = `<p>${data.location}</p>`;
+			if (data.location === null) {
+				document.getElementById('location').innerHTML = `<p>Not Available</p>`;
+			}
+			//Links
+			document.getElementById('links').innerHTML = `<p>${data.blog}</p>`;
+			if (data.blog === null) {
+				document.getElementById('links').innerHTML = `<p>Not Available</p>`;
+			}
+			//twitter
+			document.getElementById(
+				'twitter'
+			).innerHTML = `<p>${data.twitter_username}</p>`;
+			if (data.twitter_username === null) {
+				document.getElementById('twitter').innerHTML = `<p>Not Available</p>`;
+			}
+
+			//others
+			document.getElementById('others').innerHTML = `<p>${data.company}</p>`;
+			if (data.company === null) {
+				document.getElementById('others').innerHTML = `<p>Not Available</p>`;
+			}
+
+			document.getElementById(
+				'github-thumb'
+			).innerHTML = `<img src="${!data.avatar_url}"/>`;
 		});
 });
 
 fetch('https://api.github.com/users/octocat')
 	.then((response) => response.json())
 	.then((data) => {
+		document.getElementById(
+			'github-thumb'
+		).innerHTML = `<img src="${!data.avatar_url}"/>`;
+
 		//Name
 		document.getElementById('result-name').innerHTML = `<p>${data.name}</p>`;
 		var date = new Date(data.created_at);
@@ -59,6 +114,9 @@ fetch('https://api.github.com/users/octocat')
 		document.getElementById(
 			'twitter'
 		).innerHTML = `<p>${data.twitter_username}</p>`;
+		if (data.twitter_username === null) {
+			document.getElementById('twitter').innerHTML = `<p>Not Available</p>`;
+		}
 
 		//others
 		document.getElementById('others').innerHTML = `<p>${data.company}</p>`;
