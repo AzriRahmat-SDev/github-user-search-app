9c8fe0f9e2a413804f6d1dc7f6685961592c563b
azrirahmat <azrirahmatishardlyworking@gmail.com>
2022-01-18 13:23:53 +0800

    changes to calculation.js file

--- docs/js/calculation.js
+++ docs/js/calculation.js
@@ -3,25 +3,22 @@ function calculateAvgMarks() {
 	var class_2 = [40, 55, 60, 48, 50];
 	var class_3 = [90, 85, 30, 86, 78, 75, 54];
 
-	var marks_list = [class_1, class_2, class_3];
-	for (
-		var class_list_index = 0;
-		class_list_index < marks_list.length;
-		class_list_index++
-	) {
-		//accessing objects in marks_list array which is named class_1, class_2, class_3
+	var classes = [class_1, class_2, class_3];
+	for (var class_object = 0; class_object < classes.length; class_object++) {
+		//accessing objects in classes array which is named class_1, class_2, class_3
 		var totalMarks = 0;
-		//mutable variable declared in the start of the loop.
-		for (var i = 0; i < class_list_index[i].length; i++) {
-			//for each iteration of this loop class_list_index[i] it will show class_1[0] to class_1[1] till the end of the length.
-			totalMarks += class_list_index[i];
+		//mutable variable declared before the start of the loop.
+		//classes[class_object].length aka classes[0].length aka class_1.length is 6
+		for (var i = 0; i < classes[class_object].length; i++) {
+			//the first iteration of the loop will be pointing to the value of class_1[0] till class_1[5] and summing them up
+			totalMarks += classes[class_object][i];
 		}
 		//at the end of the loop within the each class iteration it will display the average
 		console.log(
-			`Average Mark for ${class_list_index} is ` +
-				totalMarks / class_list_index.length
+			`Average Mark for Class ${class_object + 1} is ` +
+				(totalMarks / classes[class_object].length).toFixed(2)
 		);
 	}
-
-	return 1;
 }
+
+calculateAvgMarks();
